4894f40fe20d768f818b78bb422af1c4cb1639cf
hnaufal-telkom <hnaufal@student.telkomuniversity.ac.id>
2025-03-24 05:28:06 +0700

    implementasi design by contract

--- tpmodul6_103022300163/Program.cs
+++ tpmodul6_103022300163/Program.cs
@@ -1,16 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics.Contracts;
+
 class SayaTubeVideo {
   private int id;
   private string title;
   private int playCount;
 
   public SayaTubeVideo(string title) {
+    Contract.Requires(title.Length <= 100 && title != null);
+    // Saya buat if statement soalnya contract nya gabisa dipake di project saya entah kenapa, ngga tau gimana setting nya
+    if (title.Length > 100 || title == null) {
+      throw new ArgumentException("Judul tidak boleh kosong dan tidak lebih dari 100 karakter");
+    }
+
     this.title = title;
     id = (new Random()).Next(5);
     playCount = 0;
   }
   public void IncreasePlayCount(int count) {
-    playCount += count;
+    Contract.Requires(count <= 10000000);
+    if (count > 10000000) {
+      throw new ArgumentException("Tidak dapat menambah PlayCount lebih dari 10000000 dalam sekali pemanggilan");
+    }
+
+    try {
+      checked {
+        playCount += count;
+      }
+    } catch (OverflowException e) {
+      Console.WriteLine(e.Message);
+    }
   }
   public void PrintVideoDetails() {
     Console.WriteLine("Detail Video");
@@ -23,7 +42,9 @@ class SayaTubeVideo {
 class Program {
   static void Main(string[] args) {
     SayaTubeVideo sayaVideo = new SayaTubeVideo("Tutorial Design By Contract - Hafidz Naufal Pradana");
-    sayaVideo.IncreasePlayCount(42);
+    for (int i = 0; i < 215; i++) {
+      sayaVideo.IncreasePlayCount(10000000);
+    }
     sayaVideo.PrintVideoDetails();
   }
 }
