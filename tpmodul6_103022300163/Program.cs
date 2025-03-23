69dad5003ce660c5222980cb095745b09f4190bb
hnaufal-telkom <hnaufal@student.telkomuniversity.ac.id>
2025-03-24 03:19:32 +0700

    menambahkan class SayaTubeVideo

--- tpmodul6_103022300163/Program.cs
+++ tpmodul6_103022300163/Program.cs
@@ -1,2 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+class SayaTubeVideo {
+  private int id;
+  private string title;
+  private int playCount;
+
+  public SayaTubeVideo(string title) {
+    this.title = title;
+    id = (new Random()).Next(5);
+    playCount = 0;
+  }
+  public void IncreasePlayCount(int count) {
+    playCount += count;
+  }
+  public void PrintVideoDetails() {
+    Console.WriteLine("Detail Video");
+    Console.WriteLine("ID Video: " + id);
+    Console.WriteLine("Judul Video: " + title);
+    Console.WriteLine("Jumlah Pemutaran Video: " + playCount);
+  }
+}
+
+class Program {
+  static void Main(string[] args) {
+    SayaTubeVideo sayaVideo = new SayaTubeVideo("Tutorial Design By Contract - Hafidz Naufal Pradana");
+    sayaVideo.IncreasePlayCount(42);
+    sayaVideo.PrintVideoDetails();
+  }
+}
